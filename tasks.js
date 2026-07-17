b59e19cecb3bf724ace4afed6e425eb6a9ee6da8
sahiba <25sahiba03@gmail.com>
2026-07-17 23:31:36 +0530

    add logic for showing current task

--- tasks.js
+++ tasks.js
@@ -9,43 +9,65 @@ let taskDueDate = document.querySelector(".taskDueDate");
 let createBtn = document.querySelector(".createButton");
 
 let activeTaskList = document.querySelector(".activeTaskList");
-  
+
 let backlogTasks = [];
 let activeTasks = [];
 let progressTasks = [];
 let completedTasks = [];
 let expireTasks = [];
 
-createBtn.addEventListener("click",function(){
-    let currentTaskvalue = taskName.value;
-    let currentTaskDueDate = taskDueDate.value;
-    let currentTaskCreateDate = new Date().toDateString();
-    
-      if(currentTaskvalue && currentTaskDueDate){
-         var currentTaskDetail = {taskName:currentTaskvalue,
-          taskCreateDate:currentTaskCreateDate,
-           taskDueDate:currentTaskDueDate ,
-           taskStatus:"active"};
-        activeTasks.push(currentTaskDetail);
-            taskName.value="";
-            taskDueDate.value="";
-            taskCreated();
-      }
-})
+createBtn.addEventListener("click", function () {
+  let currentTaskvalue = taskName.value;
+  let currentTaskDueDate = taskDueDate.value;
+  let currentTaskCreateDate = new Date().toDateString();
 
-function taskCreated(){
-  activeTaskList.innerHTML = "";
-  for(let i=0; i<activeTasks.length; i++){
-    if(activeTasks[i].taskStatus === "active"){
-    activeTaskList.insertAdjacentHTML("beforeend",`
-      <li class="taskListItemDetails"> 
-      <span>${activeTasks[i].taskName}</span>
-      <span>${activeTasks[i].taskCreateDate}</span>
-      <span>${activeTasks[i].taskDueDate}</span>
-      <span>${activeTasks[i].taskStatus}<span>
-      </li> 
-      `);
+  if (currentTaskvalue && currentTaskDueDate) {
+    var currentTaskDetail = {
+      taskName: currentTaskvalue,
+      taskCreateDate: currentTaskCreateDate,
+      taskDueDate: currentTaskDueDate,
+      taskStatus: "active",
+    };
+    activeTasks.push(currentTaskDetail);
+    taskName.value = "";
+    taskDueDate.value = "";
+    let active = "active";
+    taskCreated(activeTaskList, activeTasks, active, activeTaskList);
+  }
+});
+
+// function taskCreated(currentTask, currentTaskArr, active, currentTaskList) {
+//   currentTask.innerHTML = "";
+//   for (let i = 0; i < currentTaskArr.length; i++) {
+//     if (currentTaskArr[i].taskStatus === active) {
+//       taskDetail(currentTaskArr, i);
+//     }
+//   }
+// }
+function taskCreated(currentTask, currentTaskArr, active, currentTaskList) {
+  currentTask.innerHTML = "";
+
+  for (let i = 0; i < currentTaskArr.length; i++) {
+    if (currentTaskArr[i].taskStatus === active) {
+      taskDetail(currentTaskArr, i, currentTaskList);
+    }
   }
 }
-}
 
+function taskDetail(currentTaskArr, i, currentTaskList) {
+  currentTaskList.insertAdjacentHTML(
+    "beforeend",
+    `
+      <li class="taskListItemDetails"> 
+      <span>${currentTaskArr[i].taskName}</span>
+      <span>${currentTaskArr[i].taskCreateDate}</span>
+      <span>${currentTaskArr[i].taskDueDate}</span>
+      <span>${currentTaskArr[i].taskStatus}<span>
+      <span>${"-^-"}</span>
+      <span>${"->-"}</span>
+      <span>${"-<-"}</span>
+      <span>${"-X-"}</span>     
+      </li> 
+      `,
+  );
+}
