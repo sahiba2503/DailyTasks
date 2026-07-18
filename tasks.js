82886a65eb26069b6a9140123dd7c85c8cb6401d
sahiba <25sahiba03@gmail.com>
2026-07-18 21:18:48 +0530

    new feature add to remove and update task

--- tasks.js
+++ tasks.js
@@ -9,43 +9,67 @@ let taskDueDate = document.querySelector(".taskDueDate");
 let createBtn = document.querySelector(".createButton");
 
 let activeTaskList = document.querySelector(".activeTaskList");
-  
-let backlogTasks = [];
-let activeTasks = [];
-let progressTasks = [];
-let completedTasks = [];
-let expireTasks = [];
+let progressTaskList = document.querySelector(".progressTaskList");
+let backlogTaskList = document.querySelector(".backlogTaskList");
+let completedTaskList = document.querySelector(".completedTaskList");
+let expireTaskList = document.querySelector(".expireTaskList");
 
-createBtn.addEventListener("click",function(){
-    let currentTaskvalue = taskName.value;
-    let currentTaskDueDate = taskDueDate.value;
-    let currentTaskCreateDate = new Date().toDateString();
-    
-      if(currentTaskvalue && currentTaskDueDate){
-         var currentTaskDetail = {taskName:currentTaskvalue,
-          taskCreateDate:currentTaskCreateDate,
-           taskDueDate:currentTaskDueDate ,
-           taskStatus:"active"};
-        activeTasks.push(currentTaskDetail);
-            taskName.value="";
-            taskDueDate.value="";
-            taskCreated();
-      }
-})
 
-function taskCreated(){
+
+let DailyTasklist = [];
+
+
+createBtn.addEventListener("click", function () {
+  let currentTaskvalue = taskName.value;
+  let currentTaskDueDate = taskDueDate.value;
+  let currentTaskCreateDate = new Date().toDateString();
+
+  if (currentTaskvalue && currentTaskDueDate) {
+    var currentTaskDetail = {
+      taskName: currentTaskvalue,
+      taskCreateDate: currentTaskCreateDate,
+      taskDueDate: currentTaskDueDate,
+      taskStatus: "active",
+    };
+    DailyTasklist.push(currentTaskDetail);
+    taskName.value = "";
+    taskDueDate.value = "";
+    taskCreated( );
+  }
+});
+
+function taskCreated() {
   activeTaskList.innerHTML = "";
-  for(let i=0; i<activeTasks.length; i++){
-    if(activeTasks[i].taskStatus === "active"){
-    activeTaskList.insertAdjacentHTML("beforeend",`
-      <li class="taskListItemDetails"> 
-      <span>${activeTasks[i].taskName}</span>
-      <span>${activeTasks[i].taskCreateDate}</span>
-      <span>${activeTasks[i].taskDueDate}</span>
-      <span>${activeTasks[i].taskStatus}<span>
-      </li> 
-      `);
+ 
+  for (let i = 0; i < DailyTasklist.length; i++) {
+   
+    displayTaskDetail(i);
   }
 }
+function displayTaskDetail(i) {
+  activeTaskList.insertAdjacentHTML(
+    "beforeend",
+    `
+      <li class="taskListItemDetails"> 
+      <span>${DailyTasklist[i].taskName}</span>
+      <span>${DailyTasklist[i].taskCreateDate}</span>
+      <span>${DailyTasklist[i].taskDueDate}</span>
+      <span>${DailyTasklist[i].taskStatus}</span>     
+      <span onclick="updateItem(${i})">⬆️</span>     
+      <span onclick="deleteItem(${i})">❌</span>
+      </li> 
+      `,
+  );
 }
+//  <span>➡️</span> <span >⬅️</span>
+function updateItem(index) {
+taskName.value = DailyTasklist[index].taskName;
+taskDueDate.value = DailyTasklist[index].taskDueDate;
 
+    taskCreated();
+}
+function deleteItem(index) {
+   DailyTasklist.splice(index,1);
+
+    taskCreated();
+}
